aa17cc52c4f8dbf51fb2bb157813f913be519d87
Ukam <ukam@Xubuntu.myguest.virtualbox.org>
2024-04-18 21:48:18 +0000

    Add index.html and javascript.js and style.css and README.md

--- javascript.js
+++ javascript.js
@@ -106,32 +106,32 @@ function winNer (arr,currentSign, currentPlayer) {
 
     if (a) {
             if (arr[0] == currentSign ) {
-                playerTurn.textContent = `The winner is ${currentPlayer}`;}
+                playerTurn.textContent = `The winner is ${currentPlayer}!`;}
     } else if (b) {
         if (arr[3] == currentSign ) {
-            playerTurn.textContent = `The winner is ${currentPlayer}`;}
+            playerTurn.textContent = `The winner is ${currentPlayer}!`;}
     } else if (c) {
         if (arr[6] == currentSign ) {
-            playerTurn.textContent = `The winner is ${currentPlayer}`;}
+            playerTurn.textContent = `The winner is ${currentPlayer}!`;}
     } else if (d) {
         if (arr[0] == currentSign ) {
-            playerTurn.textContent = `The winner is ${currentPlayer}`;}
+            playerTurn.textContent = `The winner is ${currentPlayer}!`;}
     } else if (e) {
         if (arr[1] == currentSign ) {
-            playerTurn.textContent = `The winner is ${currentPlayer}`;}
+            playerTurn.textContent = `The winner is ${currentPlayer}!`;}
     } else if ( f) {
         if (arr[2] == currentSign ) {
-            playerTurn.textContent = `The winner is ${currentPlayer}`;}
+            playerTurn.textContent = `The winner is ${currentPlayer}!`;}
     } else if (g) {
         if (arr[0] == currentSign ) {
-            playerTurn.textContent = `The winner is ${currentPlayer}`;}
+            playerTurn.textContent = `The winner is ${currentPlayer}!`;}
     } else if (h) {
         if (arr[2] == currentSign ) {
-            playerTurn.textContent = `The winner is ${currentPlayer}`;}
+            playerTurn.textContent = `The winner is ${currentPlayer}!`;}
     }
     else if (arr[0] !== '' && arr[1] !== '' && arr[2] !== '' && arr[3] !== '' && arr[4] !== '' 
     && arr[5] !== '' && arr[6] !== '' && arr[7] !== '' && arr[8] !== ''){
-        playerTurn.textContent = 'The game is a draw';
+        playerTurn.textContent = 'The game is a draw!';
     }
     return false;
 }
